c3a58419d39acaf5f13f980bdb155cb4df43d900
mircool <ynyjyz@163.com>
2024-12-17 16:28:30 +0800

    perf(django_aieditor): 优化富文本编辑器的初始化和错误处理
    - 更新版本号至 0.1.13
    - 重构编辑器初始化逻辑，确保在 DOM 加载完成后执行
    - 添加错误处理机制，提高代码健壮性
    - 优化 HTML 结构，为编辑器和原始 textarea 添加包装容器

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with io.open("README.md", encoding='utf-8') as f:
 
 setup(
     name="django-aieditor",
-    version="0.1.12",
+    version="0.1.13",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
