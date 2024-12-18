9d3b432f413d04c3f33f82bef66a01bc49c4c301
mircool <ynyjyz@163.com>
2024-12-18 20:17:20 +0800

    chore: 更新版本号到1.0.12

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with io.open("README.md", encoding='utf-8') as f:
 
 setup(
     name="django-aieditor",
-    version="1.0.11",
+    version="1.0.12",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
